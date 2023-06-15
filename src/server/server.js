c619db6b7dbb34102d36943867c0a58e2f8b896e
sehgxl <abhinavsehgal802@gmail.com>
2023-06-15 19:52:24 +0530

    Add cors

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./router.js";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json()); //allows client to send us json basically or else you would have to manually put the bits together
 app.use(express.urlencoded({ extended: true }));
